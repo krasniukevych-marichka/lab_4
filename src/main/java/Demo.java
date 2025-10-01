a7588cb7f17b1df3413cec4a2bc84d81940e0f06
krasniukevych-marichka <m.krasniukevych@gmail.com>
2025-10-01 14:55:20 +0300

    Added implementation of a fight of two characters

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,5 +1,17 @@
+import ucu.edu.ua.CharacterFactory;
+import ucu.edu.ua.GameManager;
+
+import java.lang.reflect.InvocationTargetException;
+import ucu.edu.ua.Character;
 public class Demo {
-    public static void main(String args[]) {
-        System.out.println("Hello, world!");
+    public static void main(String args[]) 
+        throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        Character c1 = CharacterFactory.createCharacter();
+        Character c2 = CharacterFactory.createCharacter();
+        GameManager.fight(c1, c2);
+
     }
 }
+
+

766ffbc0704e01a9b35f2541ed7c96eae4a258fc
krasniukevych-marichka <m.krasniukevych@gmail.com>
2025-10-01 14:50:16 +0300

    Implemented Character class

---
+++ src/main/java/ucu/edu/ua/Character.java
@@ -0,0 +1,25 @@
+package ucu.edu.ua;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public abstract class Character {
+    private int power;
+    private int hp;
+    private KickStrategy kickStrategy;
+
+    public boolean isAlive() {
+        return hp > 0;
+    }
+
+    public void kick(Character enemy) {
+        kickStrategy.kick(this, enemy);
+
+    }
+}
+
+
+
+
